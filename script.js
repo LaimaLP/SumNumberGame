9432bb8bae944ab4b40921829f53128e16a4f49c
Laima <pileliene.laima@gmail.com>
2023-11-10 01:32:51 +0200

    show

--- script.js
+++ script.js
@@ -1,15 +1,87 @@
 
 // const sumDOM = 
 
-let answerDOM = document.getElementById('answer').value;
+const answerDOM = document.getElementById('answer')
+const buttonDOM = document.getElementById('btn')
+
+const resultDOM =document.getElementById("result") ;
+const resultValue =document.getElementById("result-value");
+
+let answerOptions = ["Valiooo :)", "Bandyk dar kartą :("];
 
 
-function InputAnswer(tekstas){
+let firstRandom;
+let secondRandom;
+let correctAnswerValue;
+getRandom(20)
+
+function getRandom(max){
+     firstRandom = Math.floor(Math.random()* max)
+     secondRandom = Math.floor(Math.random()* max)
     
-const answerDOM = document.getElementById('answer').innerText=`${tekstas}, teisingai`
-document.getElementById("btn").addEventListener("click", ()=>{
 
-    
-})
+    document.getElementById("number1").textContent=`${firstRandom}`
+    document.getElementById("number2").textContent=`${secondRandom}`
+
+    correctAnswerValue = firstRandom + secondRandom
+ 
 }
-InputAnswer(answerDOM)
+
+
+document.getElementById("dar").addEventListener("click", ()=>{
+    let max = 20;
+    getRandom(max)
+})
+
+buttonDOM.addEventListener("click", function(){
+    resultValue.classList.add("show");
+
+    let userInput = +answerDOM.value
+
+    if(userInput === correctAnswerValue){
+        resultValue.textContent = answerOptions[0]
+    }else{
+        resultValue.textContent = answerOptions[1]
+    }
+})
+
+
+
+
+
+
+// const offbackgroundDOM = document.querySelector(".block-container");
+// offbackgroundDOM.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     console.log("veikia")
+//     resultValue.classList.remove("show");
+//   });
+
+
+
+// const resultCDOM = document.querySelector(".result-container");
+// const offbackgroundDOM =  resultCDOM.querySelector(".offbackground");
+
+// if (buttonDOM && blockDOM) {
+//     buttonDOM.addEventListener("click", () => {
+//       asideDOM.classList.add("show");
+//     });
+  
+//     asideBackgroundDOM.addEventListener("click", () => {
+//       asideDOM.classList.remove("show");
+//     });
+  
+//     asideCloseBtnDOM.addEventListener("click", () => {
+//       asideDOM.classList.remove("show");
+//     });
+  
+//     window.addEventListener("keyup", (event) => {
+//       if (event.key === "Escape") {
+//         asideDOM.classList.remove("show");
+//       }
+//       if (event.key === "+") {
+//         asideDOM.classList.add("show");
+//       }
+//     });
+//   }
+  
