aba686b66dff7e56c6839805f90a533fc2ad2e02
Laima <pileliene.laima@gmail.com>
2023-11-09 21:14:35 +0200

    initial html structure and css

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// const sumDOM = 
+
+let answerDOM = document.getElementById('answer').value;
+
+
+function InputAnswer(tekstas){
+    
+const answerDOM = document.getElementById('answer').innerText=`${tekstas}, teisingai`
+document.getElementById("btn").addEventListener("click", ()=>{
+
+    
+})
+}
+InputAnswer(answerDOM)
